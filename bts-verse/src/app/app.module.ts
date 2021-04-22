ca4e665db8aa84c66409f43213d1bac652201d9a
rholop <57956151+rholop@users.noreply.github.com>
2021-04-21 21:37:40 -0400

    Added space for twitter-feed!

--- bts-verse/src/app/app.module.ts
+++ bts-verse/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwitterFeedComponent } from './twitter-feed/twitter-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwitterFeedComponent
   ],
   imports: [
     BrowserModule,
