949e10d727ad22a07ac5aec2eadcc9cb2d321f53
rholop <57956151+rholop@users.noreply.github.com>
2021-04-30 16:46:38 -0400

    Fixed some CSS

--- bts-verse/src/app/wikipedia/wikipedia.component.ts
+++ bts-verse/src/app/wikipedia/wikipedia.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 declare var wikipedia: any;
 
 @Component({
   selector: 'app-wikipedia',
   templateUrl: './wikipedia.component.html',
-  styleUrls: ['./wikipedia.component.css']
+  styleUrls: ['./wikipedia.component.css'],
 })
 export class WikipediaComponent implements OnInit {
 
   constructor() {
+    imports : [ BrowserModule, HttpClientModule ]
   }
 
   ngOnInit(): void {
